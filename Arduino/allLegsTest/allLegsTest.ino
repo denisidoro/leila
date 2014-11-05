e9ddbf640000499e1d4929e34272c7100bf43513
Denis <denis.isidoro-de-franca@student.ecp.fr>
2014-11-05 19:07:18 +0000

    Add AX12 library

--- Arduino/allLegsTest/allLegsTest.ino
+++ Arduino/allLegsTest/allLegsTest.ino
@@ -1,5 +1,7 @@
 #include <VarSpeedServo.h> 
 
+#define SERVO_COUNT 4;
+
 VarSpeedServo sv[12]; 
 int svt[12];
 
@@ -7,22 +9,25 @@ void setup() {
 
 	Serial.begin(9600);
 
-	for (int i = 0; i < 12; i++)
+	for (int i = 0; i < SERVO_COUNT; i++) {
 		sv.attach(2 + i, 900, 2100);
+	}
 
 }
 
 void loop() {
 
 	if (Serial.available()) {
-		for (int i = 0; i < 12; i++)
+		for (int i = 0; i < SERVO_COUNT; i++) {
 			svt[i] = Serial.parseInt();
+		}
 		updateServos();
 	}
 
 }
 
 void updateServos() {
-	for (int i = 0; i < 12; i++)
+	for (int i = 0; i < SERVO_COUNT; i++) {
 		sv[i].write(svt[i]);
+	}
 }

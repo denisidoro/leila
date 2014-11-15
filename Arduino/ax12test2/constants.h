5601eda03fdb23e554ddaf981a0f7d3f44a5f6bf
Denis <denis.isidoro-de-franca@student.ecp.fr>
2014-11-15 10:58:56 +0000

    Add Matlab leg simulation

--- Arduino/ax12test2/constants.h
+++ Arduino/ax12test2/constants.h
@@ -1,7 +1,7 @@
 // Body definitions
 #define CoxaLength			50		// in mm, 49.716mm
 #define FemurLength			83		// in mm, 82.9mm
-#define TibiaLength			145		// in mm, 144.448mm
+#define TibiaLength			144		// in mm, 144.448mm
 
 // IO pin definitions
 #define AX12Pin				2

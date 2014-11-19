3ab4b9eb28e8a13a6acfb482e05424c71dbbe119
Denis <denis.isidoro-de-franca@student.ecp.fr>
2014-11-19 19:47:59 +0000

    Remove unnecessary files

--- Arduino/duino/constants.h
+++
@@ -1,8 +0,0 @@
-// Body definitions
-#define CoxaLength			50		// in mm, 49.716mm
-#define FemurLength			83		// in mm, 82.9mm
-#define TibiaLength			144		// in mm, 144.448mm
-
-// IO pin definitions
-#define AX12Pin				2
-#define LedPin				13
